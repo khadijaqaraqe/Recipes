62847fc32f5d4b8733c20e5a82ed45c5fc373a1b
Khadija Qaraqe <khadija.k.qaraqe@gmail.com>
2021-02-01 23:22:14 +0200

    367 updates

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,11 +1,14 @@
 import { Component, ComponentFactoryResolver, OnDestroy, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
+import { Store } from "@ngrx/store";
 import { Observable, Subscription } from "rxjs";
 
 import { AlertComponent } from "../shared/alert/alert.component";
 import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
 import { AuthService, AuthResponseData } from "./auth.service";
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
 
 @Component({
     selector: 'app-auth',
@@ -21,7 +24,8 @@ export class AuthComponent implements OnDestroy {
     constructor(
         private authService: AuthService,
         private router: Router,
-        private componentFactoryResolver: ComponentFactoryResolver
+        private componentFactoryResolver: ComponentFactoryResolver,
+        private store: Store<fromApp.AppState>
     ) {}
 
     onSwitchMode() {
@@ -40,7 +44,8 @@ export class AuthComponent implements OnDestroy {
         this.isLoading = true;
 
         if (this.isLoginMode) {
-            authObs = this.authService.login(email, password);
+            // authObs = this.authService.login(email, password);
+            this.store.dispatch(new AuthActions.LoginStart({email: email, password: password}));
         } else {
             authObs = this.authService.signup(email, password);
         }
